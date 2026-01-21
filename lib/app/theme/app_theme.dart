bc3b9d8c5bcb90a99216e2925546fbbfccdfaa9a
YedynakM <mykhailo.yedynak.ki.2022@lpnu.ua>
2026-01-21 22:57:09 +0200

    Revert "feat: Bluetooth setup + dasgboard change"

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -22,8 +22,7 @@ class AppTheme {
           foregroundColor: Colors.black,
           padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
           textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          shape: RoundedRectangleBorder(borderRadius:
-           BorderRadius.circular(12)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         ),
       ),
       textTheme: const TextTheme(
